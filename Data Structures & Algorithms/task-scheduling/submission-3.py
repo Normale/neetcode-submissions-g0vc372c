426bd0bb4a65fb6d38fe9202e8cb72ad6a586a57
Normale <52136684+Normale@users.noreply.github.com>
2026-06-09 21:26:07 +0200

    Add: task-scheduling - submission-3

---
+++ Data Structures & Algorithms/task-scheduling/submission-3.py
@@ -0,0 +1,17 @@
+from collections import Counter
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        c = Counter(tasks)
+        t = 0
+        last_done = dict()
+        while True:
+            most_common = c.most_common(1)[0]
+            if most_common[1] == 0:
+                break
+            task = most_common[0]
+            # check last done
+            if t - last_done.get(task, float('-inf')) > n:
+                last_done[task] = t
+                c[task] -= 1
+            t += 1
+        return t
