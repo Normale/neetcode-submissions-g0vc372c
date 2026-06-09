a6e4bbeefd7e5d827ee5e8e6b90a94828a86bb9e
Normale <52136684+Normale@users.noreply.github.com>
2026-06-09 16:23:28 +0200

    Add: binary-tree-maximum-path-sum - submission-14

---
+++ Data Structures & Algorithms/binary-tree-maximum-path-sum/submission-14.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def dfs(self, node) -> tuple[int, int]:
+        """Returns tuple[max_value_one_branch, max_value_both_branches]."""
+        if not node.left and not node.right:
+            return node.val, node.val
+        if node.right:
+            right_val, right_exclusive_val = self.dfs(node.right)
+        else:
+            right_val, right_exclusive_val = 0, float()
+        if node.left:
+            left_val, left_exclusive_val = self.dfs(node.left)
+        else:
+            left_val, left_exclusive_val = 0, float('-inf')
+        total = node.val + max(left_val, right_val, 0)
+        
+        max_value_one_branch = total
+        max_through_current = node.val + max(0,left_val)+ max(0,right_val)
+        max_value_both_branches = max(
+            max_through_current,
+            right_exclusive_val,
+            left_exclusive_val
+        )
+        return max_value_one_branch, max_value_both_branches
+
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        result = max(self.dfs(root))        
+        print(result)
+        return result
