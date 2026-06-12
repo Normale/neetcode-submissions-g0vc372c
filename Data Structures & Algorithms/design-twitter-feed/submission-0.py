cb460ca815949760d62dd3fffd83edae5dd2d689
Normale <52136684+Normale@users.noreply.github.com>
2026-06-12 23:11:59 +0200

    Add: design-twitter-feed - submission-0

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-0.py
@@ -0,0 +1,38 @@
+class Twitter:
+
+    def __init__(self):
+        self.user_tweets = dict() # user_id -> heap of (tweet, timestamp)
+        self.follows = dict() # user_id -> list of followed
+        self.timestamp = 0
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        if userId not in self.user_tweets:
+            self.user_tweets[userId] = list()
+        item = (self.timestamp, tweetId)
+        heapq.heappush(self.user_tweets[userId], item)
+        self.timestamp -= 1
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        result = []
+        if userId not in self.follows or not self.follows[userId]:
+            return []
+        users = self.follows[userId].copy()
+        users.add(userId)
+        while len(result) < 10:
+            for user in users:
+                if user not in self.user_tweets or not self.user_tweets[user]:
+                    continue
+                top10 = heapq.nlargest(self.user_tweets[user], 10)
+                for item in top10:
+                    heapq.heappush(result, item)
+        return heapq.nlargest(result, 10)
+
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId not in self.follows:
+            self.follows[followerId] = set()
+        self.follows[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followeeId not in self.follows[followerId]:
+            return
+        self.follows[followerId].remove(followeeId)
