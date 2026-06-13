05a7a40025bfb368ba20cf6fec28fc5fbb28fc54
Normale <52136684+Normale@users.noreply.github.com>
2026-06-14 01:10:21 +0200

    Add: permutations - submission-4

---
+++ Data Structures & Algorithms/permutations/submission-4.py
@@ -0,0 +1,19 @@
+from itertools import cycle
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        results = []
+        target_len = len(nums)
+        def backtrack(path,used):
+            if len(path) == target_len:
+                results.append(path.copy())
+            
+            for i in range(target_len):
+                if used[i]:
+                    continue
+                used[i] = True
+                path.append(nums[i])
+                backtrack(path, used)
+                path.pop()
+                used[i] = False
+        backtrack([], [False] * target_len)
+        return results
