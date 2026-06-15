d78488c28c20230f4ebb50ac76b2c6f7ac8f8227
Normale <52136684+Normale@users.noreply.github.com>
2026-06-15 17:31:24 +0200

    Add: palindrome-partitioning - submission-11

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-11.py
@@ -0,0 +1,24 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        # is_pal[i][j] is True if s[i:j+1] is a palindrome
+        is_pal = [[False] * len(s) for _ in range(len(s))]
+
+        for length in range(1, len(s)):
+            for i in range(0, len(s) - length + 1):
+                j = i + length
+                if s[i] == s[j-1] and (length <= 2 or is_pal[i+1][j-1]):
+                    is_pal[i][j-1] = True
+
+        result = []
+        partition = []
+        def dfs(i):
+            if i == len(s):
+                result.append(partition.copy())
+                return
+            for j in range(i, len(s)):
+                if is_pal[i][j]:
+                    partition.append(s[i:j+1])
+                    dfs(j+1)
+                    partition.pop()
+        dfs(0)
+        return result
