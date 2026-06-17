66e6c6c5b90e1d80289b0977d2c09c77dc65f710
Normale <52136684+Normale@users.noreply.github.com>
2026-06-17 16:48:28 +0200

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.py
@@ -0,0 +1,37 @@
+class TrieNode:
+    def __init__(self):
+        self.children = dict()
+        self.end = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+
+        for letter in word:
+            if letter not in curr.children:
+                curr.children[letter] = TrieNode()
+            curr = curr.children[letter]
+        curr.end = True
+
+    def search(self, word: str) -> bool:
+
+        curr = self.root
+        def dfs(index, node):
+            if index == len(word) - 1:
+                return node.end
+
+            letter = word[index]
+            
+            if letter == ".":
+                for child in node.children.values():
+                    if dfs(index+1, child):
+                        return True
+            if letter not in curr.children:
+                return False
+            
+            return dfs(index+1, node.children[char])
+        return dfs(0, self.root)
